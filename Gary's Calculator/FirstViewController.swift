3a1d780e702056edfd10bd7707e1eef37961b161
cyrus <iosdev.cyrus@gmail.com>
2017-01-19 16:25:59 -0800

    Initial Commit

---
+++ Gary's Calculator/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Gary's Calculator
+//
+//  Created by john doe on 1/19/17.
+//  Copyright © 2017 john doe. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
