0ebf3c277e1efdad90ca57edf696bc21e3ef21cd
cyrus <iosdev.cyrus@gmail.com>
2017-02-06 07:13:16 -0800

    a

--- Gary's Calculator/CalculatorVC.swift
+++ Gary's Calculator/CalculatorVC.swift
@@ -9,14 +9,29 @@
 import UIKit
 
 
-class CalculatorVC:UIViewController {
+class CalculatorVC:UIViewController,UITextFieldDelegate{
     
+
+    @IBOutlet weak var constraintBottom: NSLayoutConstraint!
+    @IBOutlet weak var scrollView: UIScrollView!
+   
     public var calculatorTag = 0
+    
+    
     var calculatorNumber = 0
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        
+        
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        
+        self.constraintBottom.constant = 50;
+        self.view.layoutIfNeeded()
         
     }
     
@@ -26,7 +41,21 @@ class CalculatorVC:UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
-    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {   //delegate method
+        
+        let viewTemp = self.view.viewWithTag(textField.tag + 1)
+        
+        if(viewTemp == nil){
+            
+            textField.resignFirstResponder()
+            
+        } else {
+            
+            viewTemp?.becomeFirstResponder()
+            scrollView.setContentOffset(CGPoint(x: 0, y: 48 * textField.tag), animated: true)
+        }
+        
+        return true
+    }
 }
 
